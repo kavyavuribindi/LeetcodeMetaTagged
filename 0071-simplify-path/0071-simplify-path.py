2dd3375ec35fa5e7cba66217fadba9dc02d1dfb7
kavyavuribindi <89411464+kavyavuribindi@users.noreply.github.com>
2025-02-20 22:05:12 -0800

    Time: 1 ms (49.65%), Space: 17.9 MB (29.17%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,56 +1,12 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-      #  [home, user, documents, '..',Pictures]
-      #stack=[home,user,pictures]
-        pathlst=path.split('/')
         stack=[]
-        for folder in pathlst:
+        for folder in path.split('/'):
             if folder=='..':
                 if stack:
                     stack.pop()
-                else:
-                    continue
-            elif folder=='' or folder=='.':
+            elif folder in ('','.'):
                 continue
             else:
                 stack.append(folder)
         return '/'+'/'.join(stack)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #   stack=[]
-
-    #   for token in path.split('/'):
-    #     if token in ('' ,'.'):
-    #       pass
-    #     elif token=='..':
-    #       if(stack):
-    #         stack.pop()
-    #     else:
-    #       stack.append(token)
-
-    #   return '/'+'/'.join(stack)
-
-
-
-    # # stack = []
-    # # for token in path.split('/'):
-    # #     if token in ('', '.'):
-    # #         pass
-    # #     elif token == '..':
-    # #         if stack: stack.pop()
-    # #     else:
-    # #         stack.append(token)
-    # # return '/' + '/'.join(stack)
