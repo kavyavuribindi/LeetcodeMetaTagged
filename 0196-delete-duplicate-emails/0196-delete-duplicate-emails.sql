25d26f881911001c0177845763277126a0207f07
kavyavuribindi <89411464+kavyavuribindi@users.noreply.github.com>
2025-03-03 23:01:11 -0800

    Time: 1440 ms (13.01%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,7 +1,15 @@
 /* Write your T-SQL query statement below */
+-- delete from person
+-- where id in 
+-- (select a.id from (
+-- select id, row_number() over (partition by email order by id)  as rn
+-- from person)a
+-- where a.rn>1)
+
+
 delete from person
-where id in 
-(select a.id from (
-select id, row_number() over (partition by email order by id)  as rn
-from person)a
-where a.rn>1)
+where
+id not in(
+select min(id) from person
+group by email
+)
