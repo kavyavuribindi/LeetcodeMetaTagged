2e3c169edc0de78e546e2d0c19c9ba959606c3e8
kavyavuribindi <89411464+kavyavuribindi@users.noreply.github.com>
2025-03-10 21:50:55 -0700

    Time: 3729 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 
 select user_id,
-concat(upper(left(name,1)),lower(right(name,len(name)-1))) as name
-from users
-order by user_id 
+CONCAT(upper(left(name,1)),lower(right(name,len(name)-1))) AS name
+from Users
+order by user_id
